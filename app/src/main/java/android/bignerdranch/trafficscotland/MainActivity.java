531812a16b1b166b54d655520b60ea37bb82c05d
girvain <gavinross88@hotmail.co.uk>
2020-03-14 01:47:37 +0000

    dono

--- app/src/main/java/android/bignerdranch/trafficscotland/MainActivity.java
+++ app/src/main/java/android/bignerdranch/trafficscotland/MainActivity.java
@@ -1,35 +1,34 @@
 package android.bignerdranch.trafficscotland;
 
-import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.fragment.app.DialogFragment;
 
 import android.app.DatePickerDialog;
-import android.os.AsyncTask;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.DatePicker;
+import android.widget.RadioButton;
 import android.widget.TextView;
 
-import java.text.DateFormat;
-import java.text.FieldPosition;
-import java.text.ParsePosition;
 import java.util.Calendar;
-import java.util.Date;
 
 public class MainActivity extends AppCompatActivity {
 
     private TextView rssFeedText;
     private Button resultsBtn;
+    private RadioButton dateSelector;
+    private String currentRoadworksUrl = "https://trafficscotland.org/rss/feeds/roadworks.aspx";
+    private String futureRoadworksUrl = "https://trafficscotland.org/rss/feeds/plannedroadworks.aspx";
+    final String currentIncedentsUrl = "https://trafficscotland.org/rss/feeds/currentincidents.aspx";
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Button datePickerBtn = findViewById(R.id.date_button);
+        Button getFeed = findViewById(R.id.get_feed_btn);
+        RadioButton dateSelecter = findViewById(R.id.date_radio_btn);
         final TextView dateInput = findViewById(R.id.date_input);
         rssFeedText = findViewById(R.id.rss_feed);
         resultsBtn = findViewById(R.id.get_results_btn);
@@ -46,20 +45,24 @@ public class MainActivity extends AppCompatActivity {
 
         }, newCalendar.get(Calendar.YEAR), newCalendar.get(Calendar.MONTH), newCalendar.get(Calendar.DAY_OF_MONTH));
 
-        datePickerBtn.setOnClickListener(new View.OnClickListener() {
+
+        getFeed.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+                new RssFeed(rssFeedText).execute();
+            }
+        });
+        dateSelecter.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
                 StartTime.show();
             }
         });
 
-
-        final String urlSource = "https://trafficscotland.org/rss/feeds/currentincidents.aspx";
-
         resultsBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                new RssFeed(rssFeedText).execute(urlSource);
+                new RssFeed(rssFeedText).execute(currentRoadworksUrl);
             }
         });
     }
